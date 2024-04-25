5c6ca9398c28c363484ea575605575e338bb7498
Seisdani <danielayalaof@gmail.com>
2024-04-25 16:31:20 -0700

    first commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hola pobres</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFAD7',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
